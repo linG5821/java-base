8950b95e8e65fd0c026fd8d858e669d46fab4e69
linG5821 <3137168214@qq.com>
2021-07-07 10:22:29 +0800

    添加JucCondition

---
+++ src/main/java/com/ling5821/javabase/juc/JucCondition.java
@@ -0,0 +1,45 @@
+package com.ling5821.javabase.juc;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author lsj
+ * @date 2021/7/7 10:06
+ */
+public class JucCondition {
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+    public void before() {
+        lock.lock();
+        try {
+            System.out.println("before");
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void after() {
+        lock.lock();
+        try {
+            condition.await();
+            System.out.println("after");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        JucCondition jucCondition = new JucCondition();
+        executorService.execute(() -> jucCondition.after());
+        executorService.execute(() -> jucCondition.before());
+    }
+}
