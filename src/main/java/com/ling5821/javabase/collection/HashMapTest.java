86017b75536b99dcee9ce02a2eae164c4237dc91
linG <3137168214@qq.com>
2021-10-19 01:26:51 +0800

    添加HashMap和StringBuffer测试

---
+++ src/main/java/com/ling5821/javabase/collection/HashMapTest.java
@@ -0,0 +1,14 @@
+package com.ling5821.javabase.collection;
+
+import java.util.HashMap;
+
+/**
+ * @author linG
+ * @date 2021-05-28 23:45
+ */
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap<String, Object> hashMap = new HashMap<>();
+
+    }
+}
